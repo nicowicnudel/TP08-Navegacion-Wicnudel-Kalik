02b3d878cae5dbc46d985718ace17dcb0e648468
Frantag15 <48183956@ort>
2025-05-30 08:24:47 -0300

    t e r m i n a d o

--- tp-navegacion/app/tabs/perfil.tsx
+++ tp-navegacion/app/tabs/perfil.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function PerfilScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 20 }}>Bienvenido al Perfil</Text>
+      <Text style={{ fontSize: 20 }}>Este es el perfil</Text>
     </View>
   );
 }
